4356202767be947d825910d2eefb9dcd7c56ae4a
Niko Kivelä <niko@tovrleaf.com>
2017-09-08 16:02:26 +0300

    Update rprompt with error code when exists

--- setup-config.zsh
+++ setup-config.zsh
@@ -23,4 +23,14 @@ done
 PROMPT="%{$fg_bold[yellow]%}%T% %{$fg_no_bold[yellow]%} %1~%{$reset_color%}"
 ! type git_super_status &>/dev/null || PROMPT=$PROMPT'$(git_super_status)'
 PROMPT=$PROMPT"%# "
-RPROMPT="%{$fg_no_bold[green]%}%d%{$reset_color%}"
+function check_last_exit_code() {
+  local LAST_EXIT_CODE=$?
+  if [[ $LAST_EXIT_CODE -ne 0 ]]; then
+    local EXIT_CODE_PROMPT=' '
+    EXIT_CODE_PROMPT+="%{$fg[red]%}-%{$reset_color%}"
+    EXIT_CODE_PROMPT+="%{$fg_bold[red]%}$LAST_EXIT_CODE%{$reset_color%}"
+    EXIT_CODE_PROMPT+="%{$fg[red]%}-%{$reset_color%}"
+    echo "$EXIT_CODE_PROMPT"
+  fi
+}
+RPROMPT='$(check_last_exit_code)%{$fg_no_bold[green]%}%d%{$reset_color%}'
