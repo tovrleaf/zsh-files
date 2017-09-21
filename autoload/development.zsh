8a7e70af43abba9d117c73ced7ac632f95381a24
Niko Kivelä <niko@tovrleaf.com>
2017-09-21 10:45:12 +0300

    Add alias to navigate to local git root

--- autoload/development.zsh
+++ autoload/development.zsh
@@ -1,1 +1,7 @@
 alias pu="phpunit --colors --verbose"
+
+alias cdg='test "$(git rev-parse --show-cdup 2>/dev/null)" != "" && cd "$(git rev-parse --show-cdup)"'
+
+alias python='/usr/local/bin/python2'
+alias pip='/usr/local/bin/pip2'
+export PATH=$PATH:~/Library/Python/2.7/bin
