c19dc23f97d9a805233977c11c7e3ef67f1ee349
Niko Kivelä <niko@tovrleaf.com>
2018-03-14 19:24:07 +0200

    Add aws profile to promt

--- setup-config.zsh
+++ setup-config.zsh
@@ -5,8 +5,11 @@ autoload -U colors && colors
 # use builtin promts
 autoload -U promptinit && promptinit
 prompt pws
+# use hooks
+autoload -U add-zsh-hook
+# use interactive promt
+setopt prompt_subst
 
-# complete commandline switches
 setopt completealiases
 
 # why would you type 'cd dir' if you could just type 'dir'?
@@ -19,10 +22,27 @@ for conf ($(dirname $0)/autoload/*.zsh); do
   source $conf
 done
 
+
 # format prompt
 PROMPT="%{$fg_bold[yellow]%}%T% %{$fg_no_bold[yellow]%} %1~%{$reset_color%}"
-! type git_super_status &>/dev/null || PROMPT=$PROMPT'$(git_super_status)'
-PROMPT=$PROMPT"%# "
+! type git_super_status &>/dev/null || PROMPT=$PROMPT' $(git_super_status)'
+
+function add_aws_profile() {
+  local p=""
+
+  if [ "${AWS_PROFILE}" = "" ]; then
+    unset AWS_PROFILE_IS_SET
+  else
+    if [ "${AWS_PROFILE_IS_SET}" != "${AWS_PROFILE}" ]; then
+      export AWS_PROFILE_IS_SET=${AWS_PROFILE}
+      local p="$bg[yellow]%}%{$fg[white]%}aws%{$reset_color%}:{%{$fg[yellow]%}${AWS_PROFILE}%{$reset_color%}}# "
+    fi
+  fi
+
+  echo "${p}"
+}
+PROMPT=$PROMPT'$(add_aws_profile)'
+
 function check_last_exit_code() {
   local LAST_EXIT_CODE=$?
   if [[ $LAST_EXIT_CODE -ne 0 ]]; then
