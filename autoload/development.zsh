b17c9af5980e5e0083194210b820fb28b56ef283
Niko Kivelä <niko@tovrleaf.com>
2019-07-08 20:47:59 +0300

    Select AWS profile

--- autoload/development.zsh
+++ autoload/development.zsh
@@ -2,3 +2,11 @@ alias cdg='test "$(git rev-parse --show-cdup 2>/dev/null)" != "" && cd "$(git re
 
 # Add 'code' to path
 export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+
+function _selaws() {
+    select _aws_profile in $(grep '\[profile' ~/.aws/config | sed 's/\[profile \(.*\)]/\1/'); do
+        export AWS_PROFILE=$_aws_profile;
+        break;
+    done
+}
+alias selaws='_selaws'
